07ecb94100acb934a74aeea044f809ee03ecf931
kvelikov <kamen.velikov@telerik.com>
2015-04-06 15:17:17 +0300

    date picker

---
+++ app/components/date-picker/date-picker.ts
@@ -0,0 +1,10 @@
+﻿import observableModule = require("data/observable");
+
+import pagesModule = require("ui/page");
+
+import datePickerViewModelModule = require("./date-picker-view-model");
+
+export function navigatedTo(args: observableModule.EventData) {
+    var page = <pagesModule.Page>args.object;
+    page.bindingContext = page.navigationContext;
+}
