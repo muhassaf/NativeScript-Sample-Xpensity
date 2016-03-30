84450c7c4667dd3036064b2ff9f48126b14e642d
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2016-03-30 13:40:23 +0300

    Set correct source maps

--- app/views/reports/reports.js
+++ app/views/reports/reports.js
@@ -3,4 +3,4 @@ function onTap() {
     alert("Tap");
 }
 exports.onTap = onTap;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmVwb3J0cy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInJlcG9ydHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBO0lBQ0ksS0FBSyxDQUFDLEtBQUssQ0FBQyxDQUFDO0FBQ2pCLENBQUM7QUFGZSxhQUFLLFFBRXBCLENBQUEifQ==
+//# sourceMappingURL=reports.js.map
